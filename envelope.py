5b787f72380f0c08b0e32351cd16c0de66d6a86b
leo.singer@LIGO.ORG <leo.singer@LIGO.ORG@ccdd05d5-56c1-461a-bf98-85f6876523b4>
2011-06-29 02:11:04 +0000

    Moved time slice table and figure together
    git-svn-id: https://svn.ligo.caltech.edu/svn/6th-Floor-Papers/lloid_paper@1176 ccdd05d5-56c1-461a-bf98-85f6876523b4

--- envelope.py
+++ envelope.py
@@ -16,7 +16,7 @@ import sys
 # Generate plot
 from gstlal.svd_bank import read_bank
 bank = read_bank('data/svd_0_9.xml')
-pylab.figure(figsize=(3,2.5))
+pylab.figure(figsize=(2.6,2.5))
 ax = pylab.subplot(111)
 legend_artists = []
 legend_labels = []
@@ -38,5 +38,5 @@ pylab.xticks([-x.end for x in bank.bank_fragments if x.end > 10], [str(x.end) fo
 pylab.grid()
 pylab.xlabel('time relative to coalescence (s)')
 pylab.ylabel(r'strain amplitude')
-pylab.subplots_adjust(left=0.075, right=0.97, top=0.98, bottom=0.225)
+pylab.subplots_adjust(left=0.075, right=0.97, top=0.95, bottom=0.225)
 pylab.savefig(sys.argv[1])
